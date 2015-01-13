3fc20c54999f3a14f6e1085258a1270453944701
Jonathan Ortega <jortega0389@gmail.com>
2015-01-13 17:36:08 -0500

    +adding layout fragments

---
+++ app/src/main/java/ptydroid/panataxi/DateTimeActivityFragment.java
@@ -0,0 +1,21 @@
+package ptydroid.panataxi;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DateTimeActivityFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.date_time_activity_fragment, container, false);
+    }
+
+}
---
+++ app/src/main/java/ptydroid/panataxi/LocationActivityFragment.java
@@ -0,0 +1,19 @@
+package ptydroid.panataxi;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+
+public class LocationActivityFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.location_activity_fragment, container, false);
+    }
+
+}
