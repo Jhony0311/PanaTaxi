d7c67395d1ce5e843f1e7d7d2e3549655b750571
Jonathan Ortega <jortega0389@gmail.com>
2015-04-06 16:12:34 -0500

    progress adding db structure

---
+++ app/src/main/java/ptydroid/panataxi/Zone.java
@@ -0,0 +1,14 @@
+package ptydroid.panataxi;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by Jonathan on 3/26/2015.
+ */
+public class Zone extends RealmObject {
+    @PrimaryKey
+    private int id;
+
+    private String name;
+}
